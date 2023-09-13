dcb8da2a2fe2d6e820b49e27ac7da062f77639b9
luc zwyssig <104757716+lucZwyssig@users.noreply.github.com>
2023-09-13 08:36:24 +0200

    Update init.js

--- Database/init.js
+++ Database/init.js
@@ -1,9 +1,1 @@
-db = db.getSiblingDB('Communication');
-db.createCollection('Appusers');
-db.createUser({
-    user: 'john',
-    pwd: 'john',
-    roles: [
-      { role: 'readWrite', db: 'Communication' }
-    ]
-});
+

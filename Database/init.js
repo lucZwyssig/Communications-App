116b2c3be12fafe45090e77b43a3e97bdf87f7ea
luczwyssig <77100623+thefaceslayer12@users.noreply.github.com>
2023-09-13 08:40:50 +0200

    fix: updated gitignore

--- Database/init.js
+++
@@ -1,9 +0,0 @@
-db = db.getSiblingDB('Communication');
-db.createCollection('Appusers');
-db.createUser({
-    user: 'john',
-    pwd: 'john',
-    roles: [
-      { role: 'readWrite', db: 'Communication' }
-    ]
-});
